8011a7df38f56cd50fbc703b273e1a72adecf277
DanielCover <159529300+danielcover@users.noreply.github.com>
2024-10-30 17:26:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Pietro Sburrani NON e\' il mio presidente, e\' Sburenzo McSburnick');
+res.end('Pietro Sburrani NON e\' il mio presidente, NON e\' Sburenzo McSburnick, e\' suca css');
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
